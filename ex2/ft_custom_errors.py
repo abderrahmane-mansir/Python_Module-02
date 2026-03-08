7995c29aaa7ac05bf54412f217c1104e105af386
Abderrahmane MANSIR <mansir.0.abderrahmane@gmail.com>
2026-03-08 11:59:37 +0100

    ex02

---
+++ ex2/ft_custom_errors.py
@@ -0,0 +1,71 @@
+class Plant:
+    def __init__(self, name: str, age: int, status: str, water_tank: int = 10):
+        self.name = name
+        self.status = status
+        self.age = age
+        self.water_tank = water_tank
+
+    def set_status(self, status: str) -> None:
+        self.status = status
+        if status == "wilting":
+            raise PlantError(self)
+
+    def water(self, amount: int) -> None:
+        if amount < self.water_tank:
+            raise WaterError(amount)
+        else:
+            print(f"{self.name} is happy with {amount}l of water!\n")
+
+
+class GardenError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class PlantError(GardenError):
+    def __init__(self, plant: Plant):
+        super().__init__(f"The {plant.name} plant is wilting")
+
+
+class WaterError(GardenError):
+    def __init__(self, water: int):
+        super().__init__("Not enough water in the tank!\n")
+
+
+def test_plant_error(plant) -> None:
+    try:
+        plant.set_status("wilting")
+    except PlantError as e:
+        print(f"Caught PlantError: {e}\n")
+
+
+def test_water_error(plant) -> None:
+    try:
+        plant.water(5)
+    except WaterError as e:
+        print(f"Caught WaterError: {e}")
+
+
+def test_all_errors(plant) -> None:
+    try:
+        plant.set_status("wilting")
+    except GardenError as e:
+        print(f"Caught a garden error: {e}")
+
+    try:
+        plant.water(5)
+    except GardenError as e:
+        print(f"Caught a garden error: {e}")
+
+
+if __name__ == "__main__":
+    plant = Plant("tomato", 1, "healthy", 10)
+    print("=== Custom Garden Errors Demo ===", end="\n\n")
+
+    print("Testing PlantError...")
+    test_plant_error(plant)
+    print("Testing WaterError...")
+    test_water_error(plant)
+    print("Testing catching all garden errors...")
+    test_all_errors(plant)
+    print("All custom error types work correctly!")
